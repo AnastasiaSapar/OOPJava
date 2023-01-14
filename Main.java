5f147680ba3616baa62b46012e22ea049d69b18b
Anastasia <anastasiasapar04@gmail.com>
2023-01-14 23:42:56 +0200

    Prak_1 Comit_1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 0;
+        int b = 0;
+        a = (int) (20 + Math.random() * (30 - 20));
+        b = (int) (40 + Math.random() * (50 - 40));
+        System.out.println("a = " + a + ";" + " b = " + b + ";");
+    }
+}
