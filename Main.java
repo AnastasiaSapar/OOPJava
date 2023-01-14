c6bdc25f7e754d6d673514b4dfa0560c95387bf0
Anastasia <anastasiasapar04@gmail.com>
2023-01-14 23:53:44 +0200

    Prak_1 Comit_2

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ public class Main {
         a = (int) (20 + Math.random() * (30 - 20));
         b = (int) (40 + Math.random() * (50 - 40));
         System.out.println("a = " + a + ";" + " b = " + b + ";");
+        System.out.println(myNumbersProcessing(a,b));
+    }
+    static int myNumbersProcessing(int a, int b){
+        System.out.println("Suma a + b = " + (a + b));
+        System.out.println("Dobutoc a * b = " + (a * b));
+        return 0;
     }
 }
